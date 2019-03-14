f216da2fdbf8bc7842cc041493b369a16c5109d9
Austin Nash <austinnash224@gmail.com>
2019-03-14 16:16:02 -0500

    Basic Nav styles added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'category/index'
+  root 'category#index'
   get 'category/show'
   get 'category/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
 end

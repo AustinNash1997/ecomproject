d1080c9ca6aca3af02657d9415adb824537a0365
Austin Nash <austinnash224@gmail.com>
2019-03-15 18:03:28 -0500

    added category partial

--- db/migrate/20190315215134_create_categories.rb
+++ db/migrate/20190315215134_create_categories.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class CreateCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
       t.string :category_id
-      t.String :name
-      t.name :description
+      t.string :name
+      t.string :description
       t.string :image
 
       t.timestamps

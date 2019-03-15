6a3014770047e55ec3c5444701eb3e53c08f2683
Austin Nash <austinnash224@gmail.com>
2019-03-15 18:56:07 -0500

    addition of more categories and products

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Product.destroy_all
+Category.destroy_all
 AdminUser.destroy_all
+
+single_player = Category.new
+single_player.name = 'Single Player'
+single_player.category_id = '1'
+single_player.description = 'A single-player video game is a video game where input from only one player is expected throughout the course of the gaming session.'
+single_player.image = 'single_player'
+single_player.save
+single_player.errors.full_messages
+puts 'single player category created.'
+
+multiplayer = Category.new
+multiplayer.name = 'Multiplayer'
+multiplayer.category_id = '2'
+multiplayer.description = 'A multiplayer video game is a video game in which more than one person can play in the same game environment at the same time, either locally or over the internet.'
+multiplayer.image = 'multiplayer'
+multiplayer.save
+multiplayer.errors.full_messages
+puts 'multiplayer category created.'
 
 tomb_raider = Product.new
 tomb_raider.name = 'Tomb Raider'
@@ -20,5 +39,16 @@ tomb_raider.product_id = '1'
 tomb_raider.save
 tomb_raider.errors.full_messages
 puts 'Tomb_Raider Created.'
+
+black_ops = Product.new
+black_ops.name = 'Black Ops IV'
+black_ops.description = 'Call of Duty: Black Ops 4 is a multiplayer first-person shooter developed by Treyarch and published by Activision.'
+black_ops.price = '49.95'
+black_ops.image = 'BlackOpsIV'
+black_ops.category_id = '2'
+black_ops.product_id = '2'
+black_ops.save
+black_ops.errors.full_messages
+puts 'Black_Ops_IV Created.'
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?

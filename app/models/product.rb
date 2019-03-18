a6494579cb33afdbb30f90b6f7e2df02a086330b
Austin Nash <austinnash224@gmail.com>
2019-03-18 11:49:41 -0500

    Added an about page, with database migration

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@
 
 class Product < ApplicationRecord
   belongs_to :category
+
+  validates :category_id, :name, :description, :price, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'about/index'
+  resources :about
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :categories

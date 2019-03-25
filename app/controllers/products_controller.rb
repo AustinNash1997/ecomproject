d86aa3d1c4613f2fbc7367226fcec0617678596f
Austin Nash <austinnash224@gmail.com>
2019-03-25 16:37:54 -0500

    category search added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,18 @@
+# frozen_string_literal: true
+
 class ProductsController < InheritedResources::Base
+  def index
+    @products = if params[:category]
+                  Product.where(category_id: params[:category])
+
+                else
+                  Product.where(category_id:2)
+                end
+  end
 
   private
 
-    def product_params
-      params.require(:product).permit(:product_id, :category_id, :name, :description, :price, :image)
-    end
+  def product_params
+    params.require(:product).permit(:product_id, :category_id, :name, :description, :price, :image)
+  end
 end
-

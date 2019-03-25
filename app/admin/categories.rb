b9a9d0c5035f614732feb3bd3f127b8c6c26c6b0
Austin Nash <austinnash224@gmail.com>
2019-03-25 18:13:13 -0500

    more attempts at active storage...

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Category do
-permit_params :category_id, :name, :description, :image
+  permit_params :category_id, :name, :description, :image
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,9 @@ class Category < ApplicationRecord
   has_many :products
 
   validates :name, :description, presence: true
+  # has_one_attached :image
+
+  def thumbnail
+    return self.image.variant(resize:'150x210!').processed
+  end
 end

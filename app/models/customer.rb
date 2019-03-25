30df4ece64bc84198f7556a0e344227f812809cb
Austin Nash <austinnash224@gmail.com>
2019-03-25 14:08:17 -0500

    addition of devise

--- app/models/customer.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class Customer < ApplicationRecord
-  has_one_attached :image
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   get 'about/index'
   resources :about
   devise_for :admin_users, ActiveAdmin::Devise.config

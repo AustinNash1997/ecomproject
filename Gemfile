3ae7b5744cd8f6eb7fd69e0a8bb695c91ac775cc
Austin Nash <austinnash224@gmail.com>
2019-04-04 14:06:45 -0500

    More attempts to get Heroku working..

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ gem 'uglifier', '>= 1.3.0'
 # gem 'mini_racer', platforms: :ruby
 
 gem 'pg'
-
+gem 'rails_12factor', group: :production
 gem "aws-sdk-s3", require: false
 
 # Use CoffeeScript for .coffee assets and views
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ single_player.name = 'Single Player'
 single_player.id = '1'
 single_player.category_id = '1'
 single_player.description = 'A single-player video game is a video game where input from only one player is expected throughout the course of the gaming session.'
-# single_player.image.attach(io: File.open(Rails.root.join('app/assets/images/single_player.png')), filename: 'single_player.png', content_type: 'img/png')
+single_player.image.attach(io: File.open(Rails.root.join('app/assets/images/single_player.png')), filename: 'single_player.png', content_type: 'img/png')
 single_player.save
 single_player.errors.full_messages
 puts 'single player category created.'
@@ -26,7 +26,7 @@ multiplayer.name = 'Multiplayer'
 multiplayer.id = '2'
 multiplayer.category_id = '2'
 multiplayer.description = 'A multiplayer video game is a video game in which more than one person can play in the same game environment at the same time, either locally or over the internet.'
-# multiplayer.image.attach(io: File.open(Rails.root.join('app/assets/images/multiplayer.png')), filename: 'multiplayer.png', content_type: 'img/png')
+multiplayer.image.attach(io: File.open(Rails.root.join('app/assets/images/multiplayer.png')), filename: 'multiplayer.png', content_type: 'img/png')
 multiplayer.save
 multiplayer.errors.full_messages
 puts 'multiplayer category created.'
@@ -35,7 +35,7 @@ tomb_raider = Product.new
 tomb_raider.name = 'Tomb Raider'
 tomb_raider.description = 'Tomb Raider is an action-adventure video game developed by Crystal Dynamics and published by Square Enix. Tomb Raider is the tenth title in the Tomb Raider franchise, and operates as a reboot that reconstructs the origins of Lara Croft.'
 tomb_raider.price = '7.00'
-# tomb_raider.image.attach(io: File.open(Rails.root.join('app/assets/images/TombRaider.jpg')), filename: 'TombRaider.jpg', content_type: 'img/jpg')
+tomb_raider.image.attach(io: File.open(Rails.root.join('app/assets/images/TombRaider.jpg')), filename: 'TombRaider.jpg', content_type: 'img/jpg')
 tomb_raider.category_id = '1'
 tomb_raider.product_id = '1'
 tomb_raider.save
@@ -46,7 +46,7 @@ black_ops = Product.new
 black_ops.name = 'Black Ops IV'
 black_ops.description = 'Call of Duty: Black Ops 4 is a multiplayer first-person shooter developed by Treyarch and published by Activision.'
 black_ops.price = '49.95'
-# black_ops.image.attach(io: File.open(Rails.root.join('app/assets/images/BlackOpsIV.jpg')), filename: 'BlackOpsIV.jpg', content_type: 'img/jpg')
+black_ops.image.attach(io: File.open(Rails.root.join('app/assets/images/BlackOpsIV.jpg')), filename: 'BlackOpsIV.jpg', content_type: 'img/jpg')
 black_ops.category_id = '2'
 black_ops.product_id = '2'
 black_ops.save
@@ -57,7 +57,7 @@ assassins_creed_origins = Product.new
 assassins_creed_origins.name = 'Assassins Creed: Origins'
 assassins_creed_origins.description = "Assassin's Creed Origins is an action-adventure video game developed by Ubisoft Montreal and published by Ubisoft. It is the tenth major installment in the Assassin's Creed series and the successor to 2015's Assassin's Creed Syndicate."
 assassins_creed_origins.price = '30.00'
-# assassins_creed_origins.image.attach(io: File.open(Rails.root.join("app/assets/images/Assassin's_Creed_Origins_Cover_Art.png")), filename: "Assassin's_Creed_Origins_Cover_Art.png", content_type: 'img/png')
+assassins_creed_origins.image.attach(io: File.open(Rails.root.join("app/assets/images/Assassin's_Creed_Origins_Cover_Art.png")), filename: "Assassin's_Creed_Origins_Cover_Art.png", content_type: 'img/png')
 assassins_creed_origins.category_id = '1'
 assassins_creed_origins.product_id = '3'
 assassins_creed_origins.save
@@ -68,7 +68,7 @@ apex = Product.new
 apex.name = 'Apex Legends'
 apex.description = 'Apex Legends is a free-to-play battle royale game developed by Respawn Entertainment and published by Electronic Arts. However, this copy contains the battlepass!'
 apex.price = '15.00'
-# apex.image.attach(io: File.open(Rails.root.join('app/assets/images/Apex_legends_cover.jpg')), filename: 'Apex_legends_cover.jpg', content_type: 'img/jpg')
+apex.image.attach(io: File.open(Rails.root.join('app/assets/images/Apex_legends_cover.jpg')), filename: 'Apex_legends_cover.jpg', content_type: 'img/jpg')
 apex.category_id = '2'
 apex.product_id = '4'
 apex.save
@@ -79,7 +79,7 @@ rainbow = Product.new
 rainbow.name = 'Rainbow Six Siege'
 rainbow.description = "Tom Clancy's Rainbow Six Siege (often shortened to Rainbow Six Siege) is a tactical shooter video game developed by Ubisoft Montreal and published by Ubisoft. It was released worldwide for Microsoft Windows, PlayStation 4, and Xbox One on December 1, 2015. The game puts heavy emphasis on environmental destruction and cooperation between players."
 rainbow.price = '25.00'
-# rainbow.image.attach(io: File.open(Rails.root.join('app/assets/images/Tom_Clancys_Rainbow_Six_Siege.jpg')), filename: "Tom_Clancy's_Rainbow_Six_Siege.jpg", content_type: 'img/jpg')
+rainbow.image.attach(io: File.open(Rails.root.join('app/assets/images/Tom_Clancys_Rainbow_Six_Siege.jpg')), filename: "Tom_Clancy's_Rainbow_Six_Siege.jpg", content_type: 'img/jpg')
 rainbow.category_id = '2'
 rainbow.product_id = '5'
 rainbow.save
@@ -90,7 +90,7 @@ sekiro = Product.new
 sekiro.name = 'Sekiro: Shadows Die Twice'
 sekiro.description = 'Sekiro: Shadows Die Twice[a] is an action-adventure video game developed by FromSoftware and published by Activision. The game was released worldwide for Microsoft Windows, PlayStation 4, and Xbox One on March 22, 2019. The game follows a Sengoku period shinobi known as Sekiro as he attempts to take revenge on a samurai who attacked him and kidnapped his lord. The game received universal acclaim from critics.'
 sekiro.price = '80.00'
-# sekiro.image.attach(io: File.open(Rails.root.join('app/assets/images/Sekiro.jpg')), filename: 'Sekiro.jpg', content_type: 'img/jpg')
+sekiro.image.attach(io: File.open(Rails.root.join('app/assets/images/Sekiro.jpg')), filename: 'Sekiro.jpg', content_type: 'img/jpg')
 sekiro.category_id = '1'
 sekiro.product_id = '6'
 sekiro.save
